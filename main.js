0737b2bad1c4f513404c622e43143b9f2ebfd51e
John Tad <johntad110@gmail.com>
2023-05-04 14:41:43 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+String.prototype.count = function(char) {
+    let count = 0;
+
+    this.split('').forEach(item => {
+        if (item === char) {
+            count++;
+        }
+    });
+    return count;
+}
+
+function sanitize(list) {
+    sanitized_list = [];
+    list.forEach(function(num) {
+        let digits = num.toString().split('');
+        digits.map(Number).forEach((n) => {
+            sanitized_list.push(n);
+        });
+    });
+    return sanitized_list;
+}
+
+function calculate() {
+    const yourName = document.forms["info-form"]["yours"].value;
+    const loversName = document.forms["info-form"]["lovers"].value;
+
+    let love_str = (yourName + "loves" + loversName).toLowerCase();
+
+    // each unique char in love string.
+    let unique_chars = [];
+    love_str.split('').forEach(char => {
+        if (!unique_chars.includes(char)) {
+            unique_chars.push(char)
+        }
+    });
+
+    // How many times each unique char occurs in the love string.
+    let char_count = [];
+    for (const c of unique_chars) {
+        char_count.push(love_str.count(c));
+    }
+
+    do {
+        char_count = sanitize(char_count);
+        char_count[0] += char_count.pop();
+    } while (char_count.length > 2)
+
+    return `${(char_count[0] * 10) + char_count[1]}%`
+
+}
+
+function show_love_score() {
+    const score = document.querySelector(".score");
+    score.innerHTML = calculate();
+    
+    return false;
+}
+
+document.querySelector("button").onclick = show_love_score;
